8f9e1a8f23777d8245a38d364837a25a23e181b9
dw1ki <dwikikurniawanreal@gmail.com>
2024-04-23 23:40:41 +0700

    feat: adding practicum06

--- praktikum06/dbpuskesmas.sql
+++ praktikum06/dbpuskesmas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Mar 2024 pada 02.44
--- Versi server: 10.4.27-MariaDB
--- Versi PHP: 8.2.0
+-- Generation Time: Apr 23, 2024 at 06:37 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kelurahan`
+-- Table structure for table `kelurahan`
 --
 
 CREATE TABLE `kelurahan` (
@@ -34,7 +34,7 @@ CREATE TABLE `kelurahan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `kelurahan`
+-- Dumping data for table `kelurahan`
 --
 
 INSERT INTO `kelurahan` (`id`, `nama`, `kec_id`) VALUES
@@ -47,7 +47,35 @@ INSERT INTO `kelurahan` (`id`, `nama`, `kec_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pasien`
+-- Table structure for table `paramedik`
+--
+
+CREATE TABLE `paramedik` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `tmp_lahir` varchar(30) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `kategori` enum('Perawat','Bidan','Apoteker','Lainnya') DEFAULT NULL,
+  `unit_kerja_id` int(11) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `telpon` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `paramedik`
+--
+
+INSERT INTO `paramedik` (`id`, `nama`, `gender`, `tmp_lahir`, `tgl_lahir`, `kategori`, `unit_kerja_id`, `alamat`, `telpon`) VALUES
+(1, 'DR. Dwiki', 'L', 'Jakarta', '2015-04-01', 'Apoteker', 1, 'Jl. Gandul', '08997809121'),
+(3, 'Dr. Yanto', 'L', 'Bogor', '2000-01-13', 'Apoteker', 2, 'JL. Krukut', '08128918291'),
+(4, 'Yanti', 'P', 'Sawangan', '1988-04-20', 'Perawat', 4, 'JL. Ahmad Yani', '08128918291'),
+(5, 'Bidan Asih', 'P', 'Jakarta', '1981-06-14', 'Bidan', 5, 'Jl. Mendik', '08811310300');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
 --
 
 CREATE TABLE `pasien` (
@@ -63,55 +91,149 @@ CREATE TABLE `pasien` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `pasien`
+-- Dumping data for table `pasien`
 --
 
 INSERT INTO `pasien` (`id`, `kode`, `nama`, `tmp_lahir`, `tgl_lahir`, `gender`, `email`, `alamat`, `kelurahan_id`) VALUES
-(1, '001', 'Ocil', 'Jakarta', '2004-01-08', 'L', 'ocil@example.com', 'Jl. Kedamaian 1', 4),
+(1, '001', 'Ocil', 'Jakarta', '2004-01-08', '0', 'ocil@example.com', 'Jl. Kedamaian 1', 4),
 (2, '002', 'Hanna', 'Jakarta', '2003-10-06', 'P', 'hanna@example.com', 'Jl. Sakura 6', 1),
 (3, '003', 'Siska', 'Semarang', '1995-03-22', 'P', 'siska@example.com', 'Jl. Layang 22', 5),
 (4, '004', 'Aldrian', 'Solo', '1998-10-21', 'L', 'aldrian@example.com', 'Jl. Toxic 21', 3),
-(5, '005', 'Naufal', 'Bogor', '2002-05-15', 'L', 'naufal@example.com', 'Jl. Halu 15', 2);
+(5, '005', 'Naufal', 'Bogor', '2002-05-15', 'L', 'naufal@example.com', 'Jl. Halu 15', 2),
+(6, '006', 'Julian', 'Jakarta', '2024-04-11', 'L', 'julianyy@gmail.com', 'Jl. Yani', 5),
+(7, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(15, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `periksa`
+--
+
+CREATE TABLE `periksa` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `berat` double NOT NULL,
+  `tinggi` double NOT NULL,
+  `tensi` varchar(20) NOT NULL,
+  `keterangan` varchar(100) NOT NULL,
+  `pasien_id` int(11) NOT NULL,
+  `dokter_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `periksa`
+--
+
+INSERT INTO `periksa` (`id`, `tanggal`, `berat`, `tinggi`, `tensi`, `keterangan`, `pasien_id`, `dokter_id`) VALUES
+(1, '2024-03-24', 56, 73, '120', 'Sakit flu', 3, 3),
+(3, '2024-02-07', 80, 175, '120/80', 'Sakit Batuk Berdahak', 6, 3),
+(4, '2024-01-08', 56, 180, '110/80', 'Kekurangan Darah Merah', 4, 1),
+(5, '2024-04-02', 75, 165, '120/80', 'Lahiran', 3, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit_kerja`
+--
+
+CREATE TABLE `unit_kerja` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `unit_kerja`
+--
+
+INSERT INTO `unit_kerja` (`id`, `nama`) VALUES
+(1, 'Laboratorium'),
+(2, 'Poli Umum'),
+(4, 'IGD'),
+(5, 'UGD');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `kelurahan`
+-- Indexes for table `kelurahan`
 --
 ALTER TABLE `kelurahan`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `pasien`
+-- Indexes for table `paramedik`
+--
+ALTER TABLE `paramedik`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `unit_kerja_id` (`unit_kerja_id`);
+
+--
+-- Indexes for table `pasien`
 --
 ALTER TABLE `pasien`
   ADD PRIMARY KEY (`id`),
   ADD KEY `kelurahan_id` (`kelurahan_id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- Indexes for table `periksa`
+--
+ALTER TABLE `periksa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `kelurahan`
+-- AUTO_INCREMENT for table `kelurahan`
 --
 ALTER TABLE `kelurahan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `paramedik`
+--
+ALTER TABLE `paramedik`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `pasien`
+-- AUTO_INCREMENT for table `pasien`
 --
 ALTER TABLE `pasien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `periksa`
+--
+ALTER TABLE `periksa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- AUTO_INCREMENT for table `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `pasien`
+-- Constraints for table `paramedik`
+--
+ALTER TABLE `paramedik`
+  ADD CONSTRAINT `paramedik_ibfk_1` FOREIGN KEY (`unit_kerja_id`) REFERENCES `unit_kerja` (`id`);
+
+--
+-- Constraints for table `pasien`
 --
 ALTER TABLE `pasien`
   ADD CONSTRAINT `pasien_ibfk_1` FOREIGN KEY (`kelurahan_id`) REFERENCES `kelurahan` (`id`);
